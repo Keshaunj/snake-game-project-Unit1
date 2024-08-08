4bff99307e0359d115427c7abc8eba9328232625
Keshaunj <keshaunjones48@gmail.com>
2024-08-07 21:31:54 -0400

    functionality day

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-console.log("hi")
+/* constant and variables*/
 const directions = {
     up: {x: 0, y: 1},
     down: {x: 0, y: -1},
@@ -7,21 +7,76 @@ const directions = {
 };
 
 const body = document.getElementById("snakebody")
-const land = document.getElementById("board")
+const board= document.getElementById("board")
+
+let position = {x:0,y:0}; /* my location of snake on the board*/
+let stepSize = 100;
+
+
+
+/* functions*/
+
+/* function for snakes ability to navaggte through my board*/
+function moveSnake(direction) {
+     position.x += direction.x * stepSize;
+     position.y += direction.y * stepSize;
+
+
+/* makes my snake stay in the board fucntion */  
+if (position.x < 0) position.x = 0; // Prevent going left
+if (position.y < 0) position.y = 0;
+/* helping my snake stay in the board*/
+if (position.x > board.clientWidth - body.clientWidth) {
+     position.x = board.clientWidth - body.clientWidth; // Prevent going right
+ }
+ 
+ if (position.y > board.clientHeight - body.clientHeight) {
+     position.y = board.clientHeight - body.clientHeight; // Prevent going up
+ }
+/* adjsuting my snake body as it moves*/
+body.style.left = `${position.x}px`;
+body.style.bottom = `${position.y}px`;
+
+}
+/* this next function  below helps my snakebbODY STAY IN THE BOARD */    /*im assmung 0 is out of boards becuase it closer to null */
+document.addEventListener('keydown',function(event){
+    
+     switch (event.key){
+          case 'ArrowUp':
+               mvfront();
+               break;
+               case 'ArrowDown':
+                    mvdown();
+                    break;
+                    case 'ArrowLeft':
+                         mvLeft()
+                         break;
+                         case 'ArrowRight':
+                              mvright();
+                              break;
+
+     
+
+
+
+}
+});
+
+
 
 function mvfront(){
-     const directions = directions.up;
+      moveSnake(directions.up);
 
 }
 
 function mvdown(){
-     const directions= directions.down;
+     moveSnake(directions.down);
 }
 
 function mvLeft(){
-    const directions = directions.left;
+    moveSnake(directions.left);
 }
 function mvright(){
-     const directions = directions.right;
+     moveSnake(directions.right);
 }
-document.addEventListener("DOMContentLoaded")
+
